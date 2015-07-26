bb3d228f3543773c6d31d262e75836e707a211fc
Phil Peng <phil.peng@ef.com>
2015-07-26 23:16:15 +0800

    WIP

--- start.js
+++ start.js
@@ -4,49 +4,60 @@ var glob = require('glob');
 var mime = require('mime');
 
 var PORT = 8080,
-  SOURCE_PATH = '',
-  EXCLUDE_FILES = null;
+	ROOT_PATH = '';
 
 var PROP_PORT = 'port',
-  PROP_SOURCE_PATH = 'sourcePath',
-  PROP_EXCLUDE_FILES = 'excludeFiles';
+	PROP_ROOT_PATH = 'rootPath',
+	PROP_SOURCE_RULE = 'sourceRule',
+	PROP_EXCLUDE_FILES = 'excludeFiles';
 
 var configPath = __dirname + '/config.json';
 
 if (!fs.existsSync(configPath)) {
-  console.log('config.json mandatory!');
-  return;
+	console.log('config.json mandatory!');
+	return;
 }
 
 var config = JSON.parse(fs.readFileSync(__dirname + '/config.json', 'utf8'));
 
 config['port'] && (PORT = config['port']);
-if (!config[PROP_SOURCE_PATH]) {
-  console.log('"sourcePath" property in config.json mandatory!');
-  return;
+if (!config[PROP_SOURCE_RULE]) {
+	console.log('"sourcePath" property in config.json mandatory!');
+	return;
 } else {
-  var SOURCE_PATH = config[PROP_SOURCE_PATH];
+	ROOT_PATH = config[PROP_SOURCE_RULE];
 }
 
 if (config[PROP_EXCLUDE_FILES]) {
-  EXCLUDE_FILES = new RegExp(config[PROP_EXCLUDE_FILES]);
+	EXCLUDE_FILES = new RegExp(config[PROP_EXCLUDE_FILES]);
 }
 
-http.createServer(function(req, res) {
-  var data = '';
-  glob(SOURCE_PATH, function(err, files) {
-    files.forEach(function(v, i) {
-      if (!v.match(EXCLUDE_FILES)) {
-        data += fs.readFileSync(v);
-      }
-    });
-    res.writeHead(200, {
-      'Content-Type': mime.lookup(SOURCE_PATH),
-      'Cache-Control': 'no-cache, no-store, must-revalidate',
-      'Pragma': 'no-cache',
-      'Expires': 0
-    });
-    res.end(data);
-  });
+http.createServer(function (req, res) {
+	var url = req.url;
+	var rules = config['rules'];
+	var matchRule;
+	if (rules.length) {
+		rules.forEach(function (v, i) {
+			if (v['url'].indexOf(url) > -1) {
+				glob(v[PROP_SOURCE_RULE], function (err, files) {
+					files.forEach(function (v, i) {
+						if (!v.match(EXCLUDE_FILES)) {
+							data += fs.readFileSync(v);
+						}
+					});
+					res.writeHead(200, {
+						'Content-Type': mime.lookup(SOURCE_PATH),
+						'Cache-Control': 'no-cache, no-store, must-revalidate',
+						'Pragma': 'no-cache',
+						'Expires': 0
+					});
+					res.end(data);
+				});
+			}
+		});
+	}
+
+	var data = '';
+
 }).listen(PORT);
 console.log('Bundle provider started at port:' + PORT);
